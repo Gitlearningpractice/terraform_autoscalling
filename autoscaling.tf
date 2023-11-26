e5edebd87c96afd689aedb837cc12c6db17cf837
Gitlearningpractice <131306166+Gitlearningpractice@users.noreply.github.com>
2023-11-26 16:55:49 -0600

    Update autoscaling.tf

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "ASIAUQNDJNH5LHYHJL5U"
-  secret_key = "6jZpFwAakWqaCW5vrmmMYd5llLNigTXSfMEyWdLG"
-  token = "FwoGZXIvYXdzEEgaDPWf6yU2e4ct9LCCWyK3Ablo0JrOwNPvzeUkaBcZWUzizXq2/stp8953JHehsxd1KrX5gjCKVI19BLOeETowa+uLdZPBZt7PNEOLgqWMgeH2npCiX8kE3z+vfs1zVWIEpt/j9zS5WkJ3xtxzxP6lIlsRHLsTCSOM0t/m/TTu6w5eNn0ZyLStpqfuFp8rwUC6bntTaDJn5k2gDCU6NgqCIH5tYc5LxiludHReiDqAId17Bq45DNmFqxtj0dY8JlHYN5aN7tPAWCjUrI2rBjItQnLKTL2b8W7jr05/r6gQhwsE9T6fGQ8qwtSHjY1a92/2olkVuFm72IU2C8wJ"
+  access_key = "********************"
+  secret_key = "*******************"
+  token = "*************************"
   region = "us-east-1"
 }
 
